7998142b244df276bfe3818f53d3b61ee785aa65
Dipak Shedge <155218698+Im-Dipak-Shedge@users.noreply.github.com>
2026-06-19 17:36:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (64.28%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int left = 0, right = numbers.size()-1;
-        
-        while(left<right)
-        {   if((numbers[left]+numbers[right]) == target)
-            return {left+1,right+1};
-            else if((numbers[left]+numbers[right]) < target)
-            left++;
-            else 
-            right--;
-        }
-return {};
+     int i = 0;
+     int j = numbers.size()-1;
+
+     while(i<j)
+     { 
+        if(numbers[i]+numbers[j] == target) return {i+1,j+1};
+       else if(numbers[i]+numbers[j] > target)
+        {
+            j--;
+        }else { i++;}
+       
+            
+     }
+     return {};
     }
 };
